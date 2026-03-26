c7c2dc431205f3b338411ae48fc9c6b60b975a16
iforbes <iforbes@K010-MA03-2.local>
2026-03-26 09:36:19 +0700

    Fix port log with real backticks

--- server.js
+++ server.js
@@ -126,6 +126,6 @@ top_themes
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
